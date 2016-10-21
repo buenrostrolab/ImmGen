928c2773f36f93baa3ca3e85cc2623b5cca52ac8
caleblareau <caleblareau@g.harvard.edu>
2016-10-21 18:22:53 -0400

    v1 data analyses--all

--- code/Immgen_chromVar.R
+++ code/Immgen_chromVar.R
@@ -16,37 +16,38 @@ BiocParallel::register(BiocParallel::MulticoreParam(2, progressbar = FALSE))
 # Performed by Caleb Lareau, 2 October
 
 #' ## Load data
-#+ cache = TRUE, message=FALSE, warning=FALSE, echo = FALSE
-csv <-  "../data/021016_newCounts.csv.zip"
-counts <- Matrix(data.matrix(read_csv(csv)))
-peakdf <- read.table("../data/021016_ImmGen_500bpPeaks.bed")
-names(peakdf) <- c("chr", "start", "end", "name")
-peaks <- GRanges(peakdf)
-
-peaks <- get_gc(peaks, genome = BSgenome.Mmusculus.UCSC.mm10)
-praw <- peaks
-peaks <- sort(peaks)
-counts <- counts[match(peaks,praw),]
-
-#' ## Update sample names from lib -> cell name
-#+ echo = FALSE
-namesdf <- read.table("../data/libnames.txt", header = TRUE)
-namesvec <- as.character(namesdf[,2])
-names(namesvec) <- namesdf[,1]
-colnames(counts) <- unname(namesvec[colnames(counts)])
-
-#' ## These are the samples that didn't pass bias filtering
-#+ cache = TRUE, message=FALSE, warning=FALSE, echo = FALSE
-low_bias_samples <- bias_filtering(counts, peaks)
-colnames(counts)[!colnames(counts) %in% names(low_bias_samples)]
-counts <- counts[, low_bias_samples]
-
-#' ### All peaks passed the filter_peaks function
-#+ cache = TRUE, message=FALSE, warning=FALSE, echo = FALSE
-peaks_to_keep <- filter_peaks(counts, peaks, non_overlapping = TRUE)
-counts <- counts[peaks_to_keep,]
-peaks <- peaks[peaks_to_keep] 
-
+if(FALSE){
+    #+ cache = TRUE, message=FALSE, warning=FALSE, echo = FALSE
+    csv <-  "../data/021016_newCounts.csv.zip"
+    counts <- Matrix(data.matrix(read_csv(csv)))
+    peakdf <- read.table("../data/021016_ImmGen_500bpPeaks.bed")
+    names(peakdf) <- c("chr", "start", "end", "name")
+    peaks <- GRanges(peakdf)
+    
+    peaks <- get_gc(peaks, genome = BSgenome.Mmusculus.UCSC.mm10)
+    praw <- peaks
+    peaks <- sort(peaks)
+    counts <- counts[match(peaks,praw),]
+    
+    #' ## Update sample names from lib -> cell name
+    #+ echo = FALSE
+    namesdf <- read.table("../data/libnames.txt", header = TRUE)
+    namesvec <- as.character(namesdf[,2])
+    names(namesvec) <- namesdf[,1]
+    colnames(counts) <- unname(namesvec[colnames(counts)])
+    
+    #' ## These are the samples that didn't pass bias filtering
+    #+ cache = TRUE, message=FALSE, warning=FALSE, echo = FALSE
+    low_bias_samples <- bias_filtering(counts, peaks)
+    #colnames(counts)[!colnames(counts) %in% names(low_bias_samples)]
+    #counts <- counts[, low_bias_samples]
+    
+    #' ### All peaks passed the filter_peaks function
+    #+ cache = TRUE, message=FALSE, warning=FALSE, echo = FALSE
+    peaks_to_keep <- filter_peaks(counts, peaks, non_overlapping = TRUE)
+    counts <- counts[peaks_to_keep,]
+    peaks <- peaks[peaks_to_keep] 
+}
 #' ## Get motifs; compute deviations. 
 #+ cache = TRUE, message=FALSE, warning=FALSE, echo = FALSE
 bg <- get_background_peaks(counts_mat = counts, bias = peaks)
@@ -56,13 +57,17 @@ bg <- get_background_peaks(counts_mat = counts, bias = peaks)
 # load our own
 load("../data/cisbp_mm9_unique.08_Jun_2016.RData")
 
-#motif_ix <- match_pwms(pwms, peaks, genome = BSgenome.Mmusculus.UCSC.mm10)
-#deviations <- compute_deviations(counts_mat = counts, background_peaks = bg, peak_indices = motif_ix)
-#saveRDS(deviations, file = "../output/deviations.rds")
+motif_ix <- match_pwms(pwms, peaks, genome = BSgenome.Mmusculus.UCSC.mm10)
+motif_score <- match_pwms(pwms, peaks, genome = BSgenome.Mmusculus.UCSC.mm10, out = "scores")
+deviations <- compute_deviations(counts_mat = counts, background_peaks = bg, peak_indices = motif_ix)
+saveRDS(deviations, file = "../output/deviations.rds")
 deviations <- readRDS("../output/deviations.rds")
 variability <- compute_variability(deviations$z)
 labels <- TFBSTools::name(pwms[rownames(variability)])
 
+#Kmer stuff
+kmer_idx <- get_kmer_indices(peaks, genome = BSgenome.Mmusculus.UCSC.mm10)
+write.table(data.frame(data.matrix(kmer_idx)), file = "../data/161021_peaksXkmer.tsv", sep = "\t", row.names = FALSE, quote = FALSE)
 
 #' ## View plots of variable TFs and deviation heatmap
 #+ fig.width=7, fig.height=7, message = FALSE, warning = FALSE, echo=FALSE
